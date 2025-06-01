ab3edc6ff5589e94ff5f90b469b28959f1656e09
HairlessVillager <hairlessvillager@qq.com>
2025-06-01 18:27:52 +0800

    fix: 'graph'

--- src/commands/graph.rs
+++ src/commands/graph.rs
@@ -1,13 +1,14 @@
-use petgraph::algo::dijkstra;
-use petgraph::graph::{Graph, NodeIndex};
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, HashSet};
 
 use crate::{
-    object::{Commit, Head, INDEX_HASH, Index, Object, ObjectHash},
+    object::{ commit::Commit, index::Index, Object, ObjectHash, INDEX_HASH},
     storage::{StorageBackend, WrappedStorageBackend},
 };
 
-pub fn graph(backend: &WrappedStorageBackend) -> HashMap<ObjectHash, Vec<ObjectHash>> {
+type SingleDirectedGraph = HashMap<ObjectHash, HashMap<ObjectHash, i32>>;
+
+// todo: tooooo many .clone(), use Graph { node, patching, reverting } instead
+pub fn graph(backend: &WrappedStorageBackend) -> (SingleDirectedGraph, SingleDirectedGraph) { // patching, reverting
     let index = backend.get(INDEX_HASH).unwrap();
     let index = Index::deserialize(&index);
 
@@ -16,19 +17,29 @@ pub fn graph(backend: &WrappedStorageBackend) -> HashMap<ObjectHash, Vec<ObjectH
         .into_iter()
         .map(|e| e.clone())
         .collect();
-    let mut graph = HashMap::new();
+    let mut visited = HashSet::new();
+    let mut patching_graph: SingleDirectedGraph = HashMap::new();
+    let mut reverting_graph: SingleDirectedGraph = HashMap::new();
     while let Some(top) = refs_stack.pop() {
+        if visited.contains(&top) {
+            continue;
+        }
         let commit = backend.get(&top).unwrap();
         let commit = Commit::deserialize(&commit);
-        let parents = commit.get_parents().clone();
-        for parnet in &parents {
-            if !graph.contains_key(parnet) {
-                refs_stack.push(parnet.clone());
+        let edges = commit.get_edges();
+        let mut reverting_edges = HashMap::new();
+        for edge in edges {
+            if !visited.contains(&edge.commit) {
+                refs_stack.push(edge.commit.clone());
             }
+            reverting_edges.insert(edge.commit.clone(), edge.revert_cost);
+            // reverting_graph.entry(&top).or_insert(HashMap::from([(edge.commit.clone(), edge.revert_cost)]));
+            patching_graph.entry(edge.commit.clone()).and_modify(|edges|{edges.insert(top.clone(), edge.patch_cost);});
         }
-        graph.insert(top.clone(), parents);
+        reverting_graph.insert(top.clone(), reverting_edges);
+        visited.insert(top);
     }
-    graph
+    (patching_graph, reverting_graph)
 }
 
 pub fn weighted_shorted_path() {
