140a60d2f17dfcc77d68235e3bc800f45d8527d8
HairlessVillager <hairlessvillager@qq.com>
2025-06-02 19:56:13 +0800

    feat: 'object::Diff::{from_squash, patch, revert}'

--- src/object/diff.rs
+++ src/object/diff.rs
@@ -1,17 +1,20 @@
 use bincode::{Decode, Encode, decode_from_slice, encode_to_vec};
 
 use crate::{
-    diff::{Diff as DiffTrait, base::BlobDiff, file::MCADiff},
+    diff::{
+        Diff as DiffTrait,
+        base::{self, BlobDiff},
+        file::MCADiff,
+    },
     util::create_bincode_config,
 };
 
 use super::Object;
 
 #[derive(Debug, Encode, Decode)]
-#[repr(u8)]
 pub enum Diff {
-    Blob(BlobDiff) = 1,
-    Region(MCADiff) = 2,
+    Blob(BlobDiff),
+    Region(MCADiff),
 }
 
 impl Diff {
@@ -32,15 +35,38 @@ impl Diff {
     }
 
     pub fn from_squash(base: &Self, squashing: &Self) -> Self {
-        todo!()
+        match (base, squashing) {
+            (Diff::Blob(base), Diff::Blob(squashing)) => {
+                Self::Blob(BlobDiff::from_squash(base, squashing))
+            }
+            (Diff::Blob(base), Diff::Region(squashing)) => {
+                let old = base.get_old_text();
+                let new = &squashing.patch(base.get_new_text());
+                Self::Blob(BlobDiff::from_compare(old, new))
+            }
+            (Diff::Region(base), Diff::Blob(squashing)) => {
+                let old = &base.revert(squashing.get_old_text());
+                let new = squashing.get_new_text();
+                Self::Blob(BlobDiff::from_compare(old, new))
+            }
+            (Diff::Region(base), Diff::Region(squashing)) => {
+                Self::Region(MCADiff::from_squash(base, squashing))
+            }
+        }
     }
 
     pub fn patch(&self, old: &Vec<u8>) -> Vec<u8> {
-        todo!()
+        match self {
+            Self::Blob(diff) => diff.patch0(),
+            Self::Region(diff) => diff.patch(old),
+        }
     }
 
     pub fn revert(&self, new: &Vec<u8>) -> Vec<u8> {
-        todo!()
+        match self {
+            Self::Blob(diff) => diff.revert0(),
+            Self::Region(diff) => diff.revert(new),
+        }
     }
 }
 
